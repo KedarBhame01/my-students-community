7ef313db20f6afe7c0a36e0a5043e47915c20793
KedarBhame01 <kedarbhame1999@gmail.com>
2025-11-02 23:38:16 +0530

    static

--- my_students_community/my_students_community/settings.py
+++ my_students_community/my_students_community/settings.py
@@ -135,11 +135,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'my_students_community', 'static'),
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 
 
